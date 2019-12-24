432f4a926bf31bbdb178717c28b83e9e2d8c323d
Sup6 <42062456+Sup66@users.noreply.github.com>
2019-12-24 21:55:02 +0200

    Add files via upload

--- __resource.lua
+++ __resource.lua
@@ -1,5 +1,5 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
-resource_type 'gametype' { name = 'SwitZ' }
+resource_type 'gametype' { name = 'CorruptSnail' }
 
 shared_script "config.lua"
 ui_page "html/ui.html"
